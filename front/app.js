c8be24705458a481d9b1dcdb6171574222e9cab9
이연승 <leeys951006@gmail.com>
2024-07-19 16:18:55 +0900

    :memo: 이연승 : app.js 내용 수정

--- front/app.js
+++ front/app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const PORT = 3000;
 const axios = require('axios');
+const PORT = 3000;
 
 // 서버 생성
 const server = http.createServer((req, res) => {
@@ -16,23 +16,32 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
-  } else if (req.method === 'POST' && req.url === '/test') {
+  } else if (req.method === 'POST' && req.url === '/login') {
     let body = '';
     req.on('data', (chunk) => {
       body += chunk.toString();
     });
     req.on('end', () => {
-      console.log('Received data:', body);
-      // app2.js 서버로 데이터 전송
+      const parsedBody = new URLSearchParams(body);
+      const id = parsedBody.get('id');
+      const password = parsedBody.get('password');
+      console.log('Received ID:', id);
+      console.log('Received Password:', password);
+
+      // mainserver.js로 데이터 전송
       axios
-        .post('http://localhost:8000/save', { data: body })
+        .post('http://localhost:8000/authenticate', { id, password })
         .then((response) => {
-          console.log('Data sent to app2.js and processed');
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end('Data received and processed');
+          if (response.data.success) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('show');
+          } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('alert');
+          }
         })
         .catch((error) => {
-          console.error('Error sending data to app2.js:', error);
+          console.error('Error sending data to mainserver.js:', error);
           res.writeHead(500, { 'Content-Type': 'text/plain' });
           res.end('Internal Server Error');
         });
