15dea12537050c233626ee82722ccd1e39dc7179
김정수 <kjs20151240@gmail.com>
2024-07-23 17:59:31 +0900

    :fire: 김정수 : testField삭제

--- frontnext/src/app/lib/testField.ts
+++
@@ -1,9 +0,0 @@
-import extractNumbers from './replaceInt';
-
-import { getYearMonthDay, getYearMonthDayTime } from './returnDate';
-
-const ymd = getYearMonthDay();
-const ymdt = getYearMonthDayTime();
-
-console.log(ymd);
-console.log(ymdt);
