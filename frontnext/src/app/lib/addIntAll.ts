1c458b1cdad1280155a9fcfe0d1f1eb8d9e4868d
김정수 <kjs20151240@gmail.com>
2024-07-23 17:58:59 +0900

    :sparkles: 김정수 : 배열을 매개로 받아 합산하는 기능 추가

--- frontnext/src/app/lib/addIntAll.ts
+++ frontnext/src/app/lib/addIntAll.ts
@@ -1,25 +1,9 @@
 // import 문으로 익스트랙 함수 불러오기
 import extractNumbers from './replaceInt';
 
-// 가격 형식으로 변환하는 함수
+// 숫자를 가격 형식으로 변환하는 함수
 const formatPrice = (value: number): string => {
-  return value.toLocaleString('ko-KR', { minimumFractionDigits: 0 }) + ' 원';
-};
-
-// 객체에서 가격을 합산하고 포맷화하는 함수
-interface PriceObject {
-  price: string[];
-}
-
-const sumFromObject = (obj: PriceObject): string => {
-  let sum = 0;
-  const priceArray = obj.price;
-
-  for (let i = 0; i < priceArray.length; i++) {
-    sum += extractNumbers(priceArray[i]);
-  }
-
-  return formatPrice(sum);
+  return value.toLocaleString('ko-KR') + ' 원';
 };
 
 // 배열에서 가격을 합산하고 포맷화하는 함수
@@ -33,5 +17,5 @@ const sumFromArray = (array: string[]): string => {
   return formatPrice(sum);
 };
 
-// 함수들을 익스포트
-export { sumFromObject, sumFromArray };
+// 함수 익스포트
+export { sumFromArray };
