0d3e694b4c009f08fdc1dd14ef22d637a80dca73
정호연 <jea7730@gmail.com>
2024-07-23 17:28:42 +0900

    :white_check_mark: 정호연 : pull request로 인해 중간저장

--- frontnext/src/app/lib/EventListner.tsx
+++ frontnext/src/app/lib/EventListner.tsx
@@ -1,8 +1,10 @@
+
 const root = document.getElementById('root');
 root.innerHTML = 'hello world';
 
 const addBut = document.createElement('button');
 const delBut = document.createElement('button');
+console.log(root);
 
 addBut.textContent = '추가'
 delBut.textContent = "삭제"
@@ -10,13 +12,14 @@ delBut.textContent = "삭제"
 root?.appendChild(addBut);
 root?.appendChild(delBut);
 
-const tagMaker = (tagName) => {
-  return document.createElement(tagName);
-};
-console.log(tagMaker('div'));
+// const tagMaker = (tagName) => {
+//   return document.createElement(tagName);
+// };
 
 addBut.addEventListener('click', () => {
   root.style.color = 'blue'
+  // const inputAdd = tagMaker('input')
+  // root?.appendChild(inputAdd)
 });
 
 delBut.addEventListener('click', () => {
