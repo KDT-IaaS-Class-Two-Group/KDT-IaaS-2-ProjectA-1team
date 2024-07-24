ef0e2aa9870a61aed23e74e263c4f8f532f21440
정호연 <jea7730@gmail.com>
2024-07-24 11:25:39 +0900

    :egg: 정호연 : 추가 버튼을 클릭하면 <input>과 <button>이 같이 생성되도록 수정

--- frontnext/src/app/lib/EventListner.tsx
+++ frontnext/src/app/lib/EventListner.tsx
@@ -3,26 +3,31 @@ const root = document.getElementById('root') as HTMLElement;
 root.innerHTML = 'hello world';
 
 const addBut = document.createElement('button');
-const delBut = document.createElement('button');
+// const delBut = document.createElement('button');
 // console.log(root);
 
 addBut.textContent = '추가'
-delBut.textContent = "삭제"
 
 root.appendChild(addBut);
-root.appendChild(delBut);
 
 const tagMaker = (tagName:string) :Element => {
- return document.createElement(tagName);
+  return document.createElement(tagName);
 };
 
 addBut.addEventListener('click', () => {
-  // root.style.color = 'blue'
-  const inputAdd = tagMaker('input')
-  root.appendChild(inputAdd)
+  const div = tagMaker('div');
+  const inputAdd = tagMaker('input');
+  const delBut = tagMaker('button');
+  delBut.textContent = '삭제';
+
+  div.className = 'item' 
+
+  div.appendChild(inputAdd);
+  div.appendChild(delBut);
+  root.appendChild(div);
 });
 
-delBut.addEventListener('click', () => {
-  const lastInput = root.lastElementChild as HTMLElement;
-  root.removeChild(lastInput);
-});
+// delBut.addEventListener('click', () => {
+//   const lastInput = root.lastElementChild as HTMLElement;
+//   root.removeChild(lastInput);
+// });
