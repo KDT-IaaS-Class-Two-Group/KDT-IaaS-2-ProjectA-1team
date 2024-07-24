42febae3fb5e4947697d56ba198e38a53ab070fe
이연승 <leeys951006@gmail.com>
2024-07-24 13:09:31 +0900

    :memo: 이연승 : Input.js 주석 추가

--- frontnext/src/app/components/Input.js
+++ frontnext/src/app/components/Input.js
@@ -1,5 +1,11 @@
 const root = document.getElementById('root');
-
+/**
+ *
+ * @param {*} type input type
+ * @param {*} id input id
+ * @param {*} name input name
+ * @returns
+ */
 const Input = (type, id, name) => {
   let content = `<input type="${type}" id="${id}" name="${name}">`;
   root.innerHTML = content;
