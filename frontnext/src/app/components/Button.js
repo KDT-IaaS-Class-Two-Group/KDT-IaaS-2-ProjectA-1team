4c0ea75025dbe2bfbb4d6f39cc54bab0a01b45c9
이연승 <leeys951006@gmail.com>
2024-07-24 12:53:22 +0900

    :memo: 이연승 : Button.js 주석 추가

--- frontnext/src/app/components/Button.js
+++ frontnext/src/app/components/Button.js
@@ -1,6 +1,10 @@
 // 버튼 안에 내용을 동적으로 적용
 const Butt = document.createElement('button');
-
+/**
+ *
+ * @param {*} buttonText 버튼 이름
+ * @returns
+ */
 const button = (buttonText) => {
   Butt.innerText = `${buttonText}`;
   return Butt;
