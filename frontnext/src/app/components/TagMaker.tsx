c53742c14bccb027c14a3def484595a4c4ea8a51
이연승 <leeys951006@gmail.com>
2024-07-24 17:45:27 +0900

    :memo: 이연승 : TagMaker.tsx 내용 수정

--- frontnext/src/app/components/TagMaker.tsx
+++ frontnext/src/app/components/TagMaker.tsx
@@ -1,10 +1,12 @@
-/**
- *
- * @param {*} tagNames TagName 입력
- * @returns
- */
+import React from 'react';
 
-const TagMaker = (tagNames) => {
+/**
+ * 지정된 태그 이름으로 HTML 요소를 생성하는 함수
+ *
+ * @param tagNames 생성할 HTML 태그의 이름
+ * @returns 생성된 HTML 요소
+ */
+const TagMaker = (tagNames: string): HTMLElement => {
   return document.createElement(tagNames);
 };
 
