8641cc966f3638a436fef0fe5a1c7008f596e604
이연승 <leeys951006@gmail.com>
2024-07-24 16:53:30 +0900

    :memo: 이연승 : createForm.js 내용 수정

--- frontnext/src/app/components/createForm.js
+++ frontnext/src/app/components/createForm.js
@@ -1,12 +1,13 @@
-const root = document.getElementById('root');
 /**
+ * @param {*} parent 부모요소 id
  * @param {*} action form action
  * @param {*} method 메서드 종류
  */
 
-const createForm = (action, method) => {
+const createForm = (parent, action, method) => {
+  const a = document.getElementById(`${parent}`);
   let forms = `<form action="${action}" method="${method}"`;
-  root.innerHTML = forms;
+  a.innerHTML = forms;
 };
 
 export default createForm;
