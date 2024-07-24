64943859732c97a9ddfbd43daef50e7431262a2c
정호연 <jea7730@gmail.com>
2024-07-24 10:54:38 +0900

    :sparkles: 정호연 : 추가, 삭제버튼로 <input> 생성 및 삭제 이벤트리스너 기능 추가

--- frontnext/src/app/lib/EventListner.tsx
+++ frontnext/src/app/lib/EventListner.tsx
@@ -1,27 +1,28 @@
 
-const root = document.getElementById('root');
+const root = document.getElementById('root') as HTMLElement;
 root.innerHTML = 'hello world';
 
 const addBut = document.createElement('button');
 const delBut = document.createElement('button');
-console.log(root);
+// console.log(root);
 
 addBut.textContent = '추가'
 delBut.textContent = "삭제"
 
-root?.appendChild(addBut);
-root?.appendChild(delBut);
+root.appendChild(addBut);
+root.appendChild(delBut);
 
-// const tagMaker = (tagName) => {
-//   return document.createElement(tagName);
-// };
+const tagMaker = (tagName:string) :Element => {
+ return document.createElement(tagName);
+};
 
 addBut.addEventListener('click', () => {
-  root.style.color = 'blue'
-  // const inputAdd = tagMaker('input')
-  // root?.appendChild(inputAdd)
+  // root.style.color = 'blue'
+  const inputAdd = tagMaker('input')
+  root.appendChild(inputAdd)
 });
 
 delBut.addEventListener('click', () => {
-  root.style.color = 'red'
+  const lastInput = root.lastElementChild as HTMLElement;
+  root.removeChild(lastInput);
 });
