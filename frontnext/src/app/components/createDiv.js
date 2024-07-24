5cabf76b797ad1beb0ffcd8940a8d20dacd8910d
이연승 <leeys951006@gmail.com>
2024-07-24 16:53:39 +0900

    :memo: 이연승 : createDiv.js 내용 수정

--- frontnext/src/app/components/createDiv.js
+++ frontnext/src/app/components/createDiv.js
@@ -1,11 +1,12 @@
-const root = document.getElementById('root');
 /**
+ * @param {*} parent 부모 요소 id
  * @param {*} textNode div안에 내용
  */
 
-const createDiv = (textNode) => {
+const createDiv = (parent, textNode) => {
+  const a = document.getElementById(`${parent}`);
   let Divs = `<div>"${textNode}"</div>`;
-  root.innerHTML = Divs;
+  a.innerHTML = Divs;
 };
 
 export default createDiv;
