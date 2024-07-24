0873abf501a5d19f586f1d0e0afc1cb3d9d36fc3
이연승 <leeys951006@gmail.com>
2024-07-24 17:47:30 +0900

    :pencil2: 이연승 : createInput.js -> createInput.tsx 이름 수정

---
+++ frontnext/src/app/components/createInput.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/**
+ * 지정된 입력 요소를 생성하고 부모 요소 안에 삽입하는 함수
+ *
+ * @param parent 부모 요소의 ID
+ * @param type 입력 요소의 타입
+ * @param id 입력 요소의 ID
+ * @param name 입력 요소의 이름
+ */
+const createInput = (parent: string, type: string, id: string, name: string) => {
+  const a = document.getElementById(parent);
+  const content = `<input type="${type}" id="${id}" name="${name}">`;
+  if (a) {
+    a.innerHTML = content;
+  }
+};
+
+export default createInput;
