3cbc174a23ddfbac4fc43101aa616298bd292327
이연승 <leeys951006@gmail.com>
2024-07-24 14:43:56 +0900

    :memo: 이연승 : form.js 주석 추가

--- frontnext/src/app/components/form.js
+++ frontnext/src/app/components/form.js
@@ -1,4 +1,8 @@
 const root = document.getElementById('root');
+/**
+ * @param {*} action form action
+ * @param {*} method 메서드 종류
+ */
 
 const form = (action, method) => {
   let forms = `<form action="${action}" method="${method}"`;
