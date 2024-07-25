af3d9b1ae78d147c7fc2bee44b1698559d3eb789
정호연 <jea7730@gmail.com>
2024-07-25 10:11:59 +0900

    :white_check_mark: 정호연 : merge로 인한 중간 저장

--- frontnext/src/app/lib/client/form.tsx
+++ frontnext/src/app/lib/client/form.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from './button';
 import { AddInput } from './addInput';
+import { AddBut } from './addBut';
 
 export const Form = () => {
   return (
     <div>
+      <AddBut />
       <form action="/createTable" method="post">
         <AddInput />
       </form>
