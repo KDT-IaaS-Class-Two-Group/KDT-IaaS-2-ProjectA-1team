ba75e652093fd32a6aaff68e70d9f5860ae93fbf
정호연 <jea7730@gmail.com>
2024-07-25 17:30:23 +0900

    :egg: 정호연 : 수정

--- frontnext/src/app/lib/client/delBut.tsx
+++ frontnext/src/app/lib/client/delBut.tsx
@@ -1,19 +1,11 @@
-import React, { useState } from 'react';
+// DelBut.tsx code
+import React from 'react';
 
-export const Button = () => {
-  const [count, setCount] = useState(0);
+interface DelButProps {
+  id: string;
+  onDelete: (id: string) => void; // 삭제 함수 추가
+}
 
-  return (
-    <div>
-      <p>Count : {count}</p>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-        id={`item-${count}`}
-      >
-        Click Me
-      </button>
-    </div>
-  );
+export const DelBut: React.FC<DelButProps> = ({ id, onDelete }) => {
+  return <button onClick={() => onDelete(id)}>항목 삭제</button>;
 };
