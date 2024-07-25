60755ea76947c80e328e1d82f2540d2064220d9f
김정수 <kjs20151240@gmail.com>
2024-07-25 09:43:27 +0900

    :construction_worker: 김정수 : nest controller

--- servernest/src/searchdata/searchdata.controller.ts
+++ servernest/src/searchdata/searchdata.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 
-@Controller('searchdata')
-export class SearchdataController {}
+@Controller('searchData')
+export class SearchDataController {
+  @Get()
+  async findAll() {
+    const externalApiUrl = 'http://localhost:8080/searchData';
+
+    try {
+      const response = await fetch(externalApiUrl);
+      if (!response.ok) {
+        throw new HttpException(
+          'Failed to fetch data from external API',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to fetch data from external API',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
