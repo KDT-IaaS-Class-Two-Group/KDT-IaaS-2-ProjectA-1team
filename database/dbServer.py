064fa1016996412c005c681b645b4d7aee61a1ab
김정수 <kjs20151240@gmail.com>
2024-07-25 10:58:28 +0900

    :construction_worker: 김정수 : python server fastapi사용 테스트

--- database/dbServer.py
+++ database/dbServer.py
@@ -1,1 +1,32 @@
-print('dbServer.py')
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import sqlite3
+from typing import List
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # 허용할 오리진을 설정합니다.
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드를 허용합니다.
+    allow_headers=["*"],  # 모든 HTTP 헤더를 허용합니다.
+)
+
+DATABASE = 'login.db'
+
+class User(BaseModel):
+    id: str
+    password: str
+
+@app.get("/searchData", response_model=List[User])
+def read_data():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, password FROM users")
+    rows = cursor.fetchall()
+    conn.close()
+
+    users = [User(id=row[0], password=row[1]) for row in rows]
+    return users
