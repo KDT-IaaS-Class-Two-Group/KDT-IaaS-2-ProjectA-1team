39dc1a53a71475aed6c85b8d89201fb82722fd6b
정호연 <jea7730@gmail.com>
2024-07-25 13:05:13 +0900

    :sparkles: 정호연 : 항목추가 버튼을 누르면 <input> 생성

--- frontnext/src/app/lib/client/addBut.tsx
+++ frontnext/src/app/lib/client/addBut.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState, ReactElement } from 'react';
+import { AddInput } from './addInput';
 
 export const AddBut = () => {
-  return <button>추가</button>;
+  const [count, setCount] = useState(0);
+  const [inputs, setInputs] = useState<ReactElement[]>([]);
+
+  return (
+    <div>
+      <div>count : {count}</div>
+      <button
+        onClick={() => {
+          setInputs([
+            ...inputs,
+            <AddInput key={inputs.length} id={`item-${count + 1}`} />,
+          ]);
+          setCount(count + 1);
+        }}
+      >
+        항목 추가
+      </button>
+      {inputs.map((input) => input)}
+    </div>
+  );
 };
