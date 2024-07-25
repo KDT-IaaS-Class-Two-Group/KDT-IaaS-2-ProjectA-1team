fd81a18bbd6eee0d1e64e0a9d55563c964a89811
김정수 <kjs20151240@gmail.com>
2024-07-25 12:52:53 +0900

    :white_check_mark: 김정수 : 테스트를위한 merge

--- frontnext/src/app/page.tsx
+++ frontnext/src/app/page.tsx
@@ -1,18 +1,6 @@
 import Image from 'next/image';
-<<<<<<< HEAD
 import Tabs from '../app/lib/tabs';
 import FormComponent from './ui/FormComponentTestFirst';
 export default function Home() {
   return <FormComponent />;
-=======
-import DataComponent from './ui/renderTest';
-
-export default function Home() {
-  return (
-    <div>
-      <h1>데이터 요청 예제</h1>
-      <DataComponent />
-    </div>
-  );
->>>>>>> AJAXServerTest
 }
