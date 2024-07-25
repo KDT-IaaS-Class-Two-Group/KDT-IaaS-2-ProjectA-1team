62289232a01ce564084e22b5adba257e8862eeac
정호연 <jea7730@gmail.com>
2024-07-25 10:08:57 +0900

    :pencil2: 정호연 : 파일 이름 변경

--- frontnext/src/app/lib/button.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-
-// button 컴포넌트 정의
-export const Button = () => {
-  const [count, setCount] = useState(0);
-
-  return <button id={`button-${count}`}>Click Me</button>;
-};
