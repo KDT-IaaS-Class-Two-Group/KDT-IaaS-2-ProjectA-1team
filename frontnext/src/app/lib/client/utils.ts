09fcf4ae8b06a5d35333a94527ebd2ef2960d07e
정호연 <jea7730@gmail.com>
2024-07-29 16:21:56 +0900

    :egg: 정호연 : 매개변수 타입 지정

--- frontnext/src/app/lib/client/utils.ts
+++ frontnext/src/app/lib/client/utils.ts
@@ -1,4 +1,13 @@
-export const handleDelete = (id, containerRef, setCount) => {
+import React, { RefObject } from 'react';
+
+// 타입을 정의합니다.
+type SetCountFunction = (value: React.SetStateAction<number>) => void;
+
+export const handleDelete = (
+  id: number,
+  containerRef: RefObject<HTMLDivElement>,
+  setCount: SetCountFunction,
+) => {
   const element = document.getElementById(`set-${id}`);
   if (element) {
     element.remove();
@@ -6,16 +15,24 @@ export const handleDelete = (id, containerRef, setCount) => {
   }
 };
 
-export const updateLabels = (containerRef, setCount) => {
+export const updateLabels = (
+  containerRef: RefObject<HTMLDivElement>,
+  setCount: SetCountFunction,
+) => {
+  if (!containerRef.current) {
+    return; // containerRef.current가 null일 경우 함수를 종료합니다.
+  }
   const remainingSets = containerRef.current.querySelectorAll('div');
   remainingSets.forEach((set, index) => {
     const label = set.querySelector('span');
     const input = set.querySelector('input');
     const button = set.querySelector('button');
     const newId = index + 1;
-    label.textContent = `항목 ${newId}`;
-    input.id = `item-${newId}`;
-    button.onclick = () => handleDelete(newId, containerRef, setCount);
+    if (label) label.textContent = `항목 ${newId}`;
+    if (input) input.id = `item-${newId}`;
+    if (button) {
+      button.onclick = () => handleDelete(newId, containerRef, setCount);
+    }
     set.id = `set-${newId}`;
   });
   setCount(remainingSets.length);
