b2aff6916e68c0bdb3250dd9b68023fddacb4534
정호연 <jea7730@gmail.com>
2024-07-29 12:52:31 +0900

    :egg: 정호연 : 매개변수 타입 수정

--- frontnext/src/app/lib/client/addInput.ts
+++ frontnext/src/app/lib/client/addInput.ts
@@ -2,7 +2,7 @@ import TagMaker from '@/app/ui/components/TagMaker';
 /**
  * * input 태그 생성 및 세팅
  */
-export const InputSet = (id: Function) => {
+export const InputSet = (id: number) => {
   const input = TagMaker('input');
   input.setAttribute('type', 'text');
   input.setAttribute('id', `item-${id}`);
