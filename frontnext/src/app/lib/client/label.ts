dd8bfbd0e507c1ff1b45c5d06e1c5595caf5d849
정호연 <jea7730@gmail.com>
2024-07-29 13:06:56 +0900

    :sparkles: 정호연 : label tag 생성 및 세팅

---
+++ frontnext/src/app/lib/client/label.ts
@@ -0,0 +1,8 @@
+import TagMaker from '@/app/ui/components/TagMaker';
+
+export const LabelSet = (id: number) => {
+  const label = TagMaker('label');
+  label.textContent = `항목 ${id}`;
+  console.log(label);
+  return label;
+};
