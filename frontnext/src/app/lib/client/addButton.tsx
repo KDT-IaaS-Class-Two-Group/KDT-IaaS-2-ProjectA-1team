87ff5049c01f99c3fb4a9ce5e05ba04a3833c471
정호연 <jea7730@gmail.com>
2024-07-29 16:11:05 +0900

    :triangular_flag_on_post: 정호연 : 항목 추가 작업 완료

--- frontnext/src/app/lib/client/addButton.tsx
+++ frontnext/src/app/lib/client/addButton.tsx
@@ -1,22 +1,23 @@
-// AddBut.tsx
 import React, { useRef, useState } from 'react';
 import { handleDelete, updateLabels } from './utils';
 import { createSet } from './createSet';
 
 export const AddBut = () => {
   const [count, setCount] = useState(0);
-  const containerRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   const handleClick = () => {
     const id = count + 1;
     const newSet = createSet(id, handleDelete, containerRef, setCount);
-    containerRef.current.appendChild(newSet);
+
+    (containerRef.current as HTMLDivElement).appendChild(newSet);
     setCount((prev) => prev + 1);
   };
 
   return (
     <div>
       <button onClick={handleClick}>항목 추가</button>
+      <button type="submit">생성하기</button>
       <div ref={containerRef}></div>
     </div>
   );
