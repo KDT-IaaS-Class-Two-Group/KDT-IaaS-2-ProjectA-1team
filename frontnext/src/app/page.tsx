34393bb00094326ab066dbf0f6abba1943006090
구하림 <166689288+koohrim12@users.noreply.github.com>
2024-07-30 18:00:35 +0900

    :white_check_mark: 구하림 : 검색기능 정검 page.tsx 중간 저장

--- frontnext/src/app/page.tsx
+++ frontnext/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="search-test">
       <header className="header">
-        <h1>과일 이름 검색하기</h1>
+        <h1>검색하기</h1>
         <Search />
       </header>
     </div>
