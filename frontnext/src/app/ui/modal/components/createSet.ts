93ae6457bfe5ca8934ffd881203f267857eeea81
정호연 <jea7730@gmail.com>
2024-07-30 16:49:10 +0900

    :egg: 정호연 : 매개변수 타입 지정

--- frontnext/src/app/ui/modal/components/createSet.ts
+++ frontnext/src/app/ui/modal/components/createSet.ts
@@ -1,21 +1,36 @@
 // createSet.ts
-export const createSet = (id, handleDelete, containerRef, setCount) => {
+export const createSet = (
+  id: number,
+  handleDelete: (
+    id: number,
+    containerRef: React.RefObject<HTMLDivElement>,
+    setCount: React.Dispatch<React.SetStateAction<number>>,
+  ) => void,
+  containerRef: React.RefObject<HTMLDivElement>,
+  setCount: React.Dispatch<React.SetStateAction<number>>,
+) => {
   const newSet = document.createElement('div');
   newSet.id = `set-${id}`;
-  newSet.classList.add('flex', 'items-center', 'mb-2.5');
+  newSet.classList.add('flex', 'items-center', 'mb-2.5', 'w-full');
 
   const input = document.createElement('input');
   input.type = 'text';
   input.placeholder = '항목을 입력하세요.';
   input.id = `item-${id}`;
-  input.classList.add('ml-2', 'border', 'rounded', 'p-1', 'text-sm');
+  input.classList.add(
+    'flex-grow',
+    'border',
+    'rounded-lg',
+    'p-2',
+    'text-sm',
+    'mr-2',
+  );
   newSet.appendChild(input);
 
   const button = document.createElement('button');
   button.textContent = '⏤';
   button.onclick = () => handleDelete(id, containerRef, setCount);
   button.classList.add(
-    'ml-2',
     'bg-red-500',
     'text-white',
     'rounded',
