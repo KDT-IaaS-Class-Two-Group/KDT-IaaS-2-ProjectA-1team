150c8e284907979bef17c589af53964fe50668cb
구하림 <166689288+koohrim12@users.noreply.github.com>
2024-07-30 13:04:24 +0900

    :memo: 구하림 : 검색기능 테스트

--- frontnext/src/app/page.tsx
+++ frontnext/src/app/page.tsx
@@ -1,6 +1,15 @@
 import Image from 'next/image';
 import Tabs from '../app/lib/tabs';
-import RenderLoginFormSection from './ui/RenderLoginFormSection';
+// import RenderLoginFormSection from './ui/RenderLoginFormSection';
+
+import Search from './lib/search';
 export default function Home() {
-  return <RenderLoginFormSection />;
+  return (
+    <div className="search-test">
+      <header className="header">
+        <h1>과일 이름 검색하기</h1>
+        <Search />
+      </header>
+    </div>
+  );
 }
