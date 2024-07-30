4c7e1f2848042ca471d790df068f47225a0817a4
정호연 <jea7730@gmail.com>
2024-07-30 16:20:47 +0900

    :art: 정호연 : tailwind style

--- frontnext/src/app/ui/modal/components/addSets.tsx
+++ frontnext/src/app/ui/modal/components/addSets.tsx
@@ -17,7 +17,12 @@ export const AddSets = () => {
   return (
     <div>
       <div ref={containerRef}></div>
-      <button onClick={handleClick}>항목 추가</button>
+      <button
+        onClick={handleClick}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+      >
+        추가
+      </button>
     </div>
   );
 };
