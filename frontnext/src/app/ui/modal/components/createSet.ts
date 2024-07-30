879e791eadedd22ca7f90f69abffd222ffd3ea92
정호연 <jea7730@gmail.com>
2024-07-30 16:20:42 +0900

    :art: 정호연 : tailwind style

--- frontnext/src/app/ui/modal/components/createSet.ts
+++ frontnext/src/app/ui/modal/components/createSet.ts
@@ -2,9 +2,7 @@
 export const createSet = (id, handleDelete, containerRef, setCount) => {
   const newSet = document.createElement('div');
   newSet.id = `set-${id}`;
-  newSet.style.display = 'flex';
-  newSet.style.alignItems = 'center';
-  newSet.style.marginBottom = '10px';
+  newSet.classList.add('flex', 'items-center', 'mb-2.5');
 
   const label = document.createElement('span');
   label.textContent = `항목 ${id}`;
@@ -14,11 +12,21 @@ export const createSet = (id, handleDelete, containerRef, setCount) => {
   input.type = 'text';
   input.placeholder = '내용을 입력하세요.';
   input.id = `item-${id}`;
+  input.classList.add('ml-2', 'border', 'rounded', 'p-1', 'text-sm');
   newSet.appendChild(input);
 
   const button = document.createElement('button');
   button.textContent = '항목 삭제';
   button.onclick = () => handleDelete(id, containerRef, setCount);
+  button.classList.add(
+    'ml-2',
+    'bg-red-500',
+    'text-white',
+    'rounded',
+    'px-2',
+    'py-1',
+    'hover:bg-red-600',
+  );
   newSet.appendChild(button);
 
   return newSet;
