d6939e3b5e4c756fcf4408d1bde8c63d1f05bc2d
김정수 <kjs20151240@gmail.com>
2024-07-31 12:36:12 +0900

    :sparkles: 김정수 : create Condiv container역할을 하는 div태그를 생성하는 기능

--- frontnext/src/app/ui/Header/headerComponent/createConDiv.tsx
+++ frontnext/src/app/ui/Header/headerComponent/createConDiv.tsx
@@ -1,1 +1,9 @@
-console.log('createConDiv.tsx');
+import React from 'react';
+
+interface ContainerDiv {
+  children: React.ReactNode;
+}
+
+export const ConDiv: React.FC<ContainerDiv> = ({ children }) => {
+  return <div>{children}</div>;
+};
