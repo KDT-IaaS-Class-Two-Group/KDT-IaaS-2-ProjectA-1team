876f91c60cee4b4926b1827837e02b316454f27b
정호연 <jea7730@gmail.com>
2024-07-31 10:09:18 +0900

    :white_check_mark: 정호연 : merge로 인한 중간저장

--- frontnext/src/app/ui/toggle.tsx
+++ frontnext/src/app/ui/toggle.tsx
@@ -1,6 +1,15 @@
+import React, { useState } from 'react';
+
 export const Toggle = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const showToggle = () => {
+    to;
+  };
+
   return (
     <div>
+      <div>children</div>
       <button>▼</button>
     </div>
   );
