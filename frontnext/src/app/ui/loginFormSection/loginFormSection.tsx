ceb5723a6ff781f1feb36916c95ac7e811098748
김정수 <kjs20151240@gmail.com>
2024-08-01 14:40:01 +0900

    :white_check_mark: 김정수 : 예상 틀구조 제작

--- frontnext/src/app/ui/loginFormSection/loginFormSection.tsx
+++ frontnext/src/app/ui/loginFormSection/loginFormSection.tsx
@@ -1,1 +1,44 @@
-console.log('loginFormSection.tsx');
+import React, { useState } from 'react';
+
+import { callApi } from '@/app/lib/AJAX';
+
+const LoginFormSection = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <div>
+        <form onSubmit={}>
+          <div>
+            <label htmlFor="id">아이디</label>
+            <input
+              type="text"
+              name="id"
+              placeholder="id를 입력하세요"
+              value={id}
+              onChange={(e) => {
+                setId(e.target.value);
+              }}
+            />
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password를 입력하세요"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <button type="submit">로그인</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default LoginFormSection;
