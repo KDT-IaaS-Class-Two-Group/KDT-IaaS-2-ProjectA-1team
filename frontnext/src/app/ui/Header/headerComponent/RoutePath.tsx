16905579179aeaa61ef83cf81d205b201b4cd262
김정수 <kjs20151240@gmail.com>
2024-08-01 10:26:32 +0900

    :rocket: 김정수 : RoutePath.tsx 파일 생성

---
+++ frontnext/src/app/ui/Header/headerComponent/RoutePath.tsx
@@ -0,0 +1,13 @@
+export const TABROUTES = {
+  TAB_ONE: '/viewpage',
+  TAB_TWO: '/TabSetting',
+  TAB_THREE: '/InsertPage',
+  TAB_FOUR: '/',
+};
+
+export const TABMENUTEXTS = {
+  TABMENUONE: '조회',
+  TABMENUTWO: '입력',
+  TABMENUTHREE: '설정',
+  TABMENUFOUR: '로그아웃',
+};
