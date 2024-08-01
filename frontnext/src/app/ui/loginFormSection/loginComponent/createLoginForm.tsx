caf0384a56b4801ae4fe68ce5969a1f40bd91c19
김정수 <kjs20151240@gmail.com>
2024-08-01 21:44:27 +0900

    :bulb: 김정수 : className과 onSubmitFunc 추가

--- frontnext/src/app/ui/loginFormSection/loginComponent/createLoginForm.tsx
+++ frontnext/src/app/ui/loginFormSection/loginComponent/createLoginForm.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 
 interface LoginFormProps {
   children: React.ReactNode;
+  className?: string;
+  onSubmitFunc?: (e: FormEvent<HTMLFormElement>) => void;
 }
 
-const LoginForm: React.FC<LoginFormProps> = ({ children }) => {
-  return <form action="">{children}</form>;
+const LoginForm: React.FC<LoginFormProps> = ({
+  className,
+  onSubmitFunc,
+  children,
+}) => {
+  return (
+    <form className={className} onSubmit={onSubmitFunc}>
+      {children}
+    </form>
+  );
 };
+
+export default LoginForm;
