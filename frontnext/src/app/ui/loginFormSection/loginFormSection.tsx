abec640a03d41d61c5cec6f025c13a8f9bc2448b
김정수 <kjs20151240@gmail.com>
2024-08-01 22:16:05 +0900

    :recycle: 김정수 : import하여 구성

--- frontnext/src/app/ui/loginFormSection/loginFormSection.tsx
+++ frontnext/src/app/ui/loginFormSection/loginFormSection.tsx
@@ -1,44 +1,89 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
+
+import { useRouter } from 'next/navigation';
 
 import { callApi } from '@/app/lib/AJAX';
+
+import ConDiv from '../Header/headerComponent/createConDiv';
+
+import LoginForm from './loginComponent/createLoginForm';
+
+import ButtonClick from '../Header/headerComponent/createBtn';
+
+import InputValueTag from './loginComponent/createInputTag';
+
+import LoginLabelTag from './loginComponent/createLabelTag';
+
+import { FormSectionstyles } from './loginComponent/loginFormStyles';
 
 const LoginFormSection = () => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const useData = { id, password };
+    const response = await callApi('8000/searchData/login', 'POST', useData);
+
+    if (response) {
+      router.push('/viewRef');
+    } else {
+    }
+  };
 
   return (
     <>
-      <div>
-        <form onSubmit={}>
-          <div>
-            <label htmlFor="id">아이디</label>
-            <input
-              type="text"
-              name="id"
-              placeholder="id를 입력하세요"
+      <ConDiv className={FormSectionstyles.container}>
+        <LoginForm
+          className={FormSectionstyles.form}
+          onSubmitFunc={handleOnSubmit}
+        >
+          <ConDiv className={FormSectionstyles.labelInputContainer}>
+            <LoginLabelTag
+              className={FormSectionstyles.label}
+              htmlFor={'id'}
+              value={'아이디'}
+            />
+            <InputValueTag
+              className={FormSectionstyles.input}
+              type={'text'}
+              name={'id'}
               value={id}
-              onChange={(e) => {
+              placeholder={'ID를 입력하세요'}
+              onChangeFunc={(e) => {
                 setId(e.target.value);
               }}
             />
-            <label htmlFor="password">비밀번호</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="password를 입력하세요"
+          </ConDiv>
+          <ConDiv className={FormSectionstyles.labelInputContainer}>
+            <LoginLabelTag
+              className={FormSectionstyles.label}
+              htmlFor={'password'}
+              value={'비밀번호'}
+            />
+            <InputValueTag
+              className={FormSectionstyles.input}
+              type={'password'}
+              name={'password'}
               value={password}
-              onChange={(e) => {
+              placeholder={'password를 입력하세요'}
+              onChangeFunc={(e) => {
                 setPassword(e.target.value);
               }}
             />
-          </div>
-          <div>
-            <button type="submit">로그인</button>
-          </div>
-        </form>
-      </div>
+          </ConDiv>
+          <ConDiv className={FormSectionstyles.messageContainer}>
+            <ButtonClick
+              className={FormSectionstyles.button}
+              type={'submit'}
+              textNode="로그인"
+            />
+          </ConDiv>
+        </LoginForm>
+      </ConDiv>
     </>
   );
 };
