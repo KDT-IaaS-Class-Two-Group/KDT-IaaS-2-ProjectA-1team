56a44919801034c550903ca7c5e4b9ae51b7cf3c
김정수 <kjs20151240@gmail.com>
2024-08-01 10:31:30 +0900

    :white_check_mark: 김정수 : test를 위한 이동

--- frontnext/src/app/viewRef/page.tsx
+++ frontnext/src/app/viewRef/page.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
+import HeaderSection from '../ui/Header/headerSection';
+import '../globals.css';
 
 export default function Home() {
-  return <h1>Hellow View</h1>;
+  return;
+  <div>
+    <HeaderSection />
+    <h1>Hellow View</h1>;
+  </div>;
 }
