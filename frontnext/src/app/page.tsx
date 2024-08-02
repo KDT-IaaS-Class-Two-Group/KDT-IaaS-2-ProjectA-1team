11eb9f3d2ca5601ec2d242a51ed7e89d172db66a
정호연 <jea7730@gmail.com>
2024-08-02 11:41:11 +0900

    :egg: 정호연 : 렌더링 되는 모듈 수정

--- frontnext/src/app/page.tsx
+++ frontnext/src/app/page.tsx
@@ -9,8 +9,7 @@ import SearchComponent from './lib/SearchComponent';
 export default function Home() {
   return (
     <div>
-      <h1>hrim+숫자1~3 을 검색 하세요.</h1>
-      <SearchComponent />
+      <RenderLoginFormSection />
     </div>
   );
 }
