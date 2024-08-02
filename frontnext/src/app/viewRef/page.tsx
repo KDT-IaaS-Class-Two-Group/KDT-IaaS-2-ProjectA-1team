52de3723dd0a4fe594ef46072e1ea3ac431554a5
정호연 <jea7730@gmail.com>
2024-08-02 12:13:28 +0900

    :egg: 정호연 : toggle component 추가

--- frontnext/src/app/viewRef/page.tsx
+++ frontnext/src/app/viewRef/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import HeaderSection from '../ui/Header/headerSection';
+import { TabToggle } from '../ui/toggle/tabToggle';
 
 export default function Home() {
   return (
     <div>
       <HeaderSection />
       <main>
-        <h1>Hellow View</h1>
+        {/* <h1>Hellow View</h1> */}
+        <TabToggle />
       </main>
     </div>
   );
