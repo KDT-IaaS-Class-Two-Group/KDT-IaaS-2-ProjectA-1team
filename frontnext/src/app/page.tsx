cd2177762277bd2e91c93f652db7f7aa72d38bf4
구하림 <166689288+koohrim12@users.noreply.github.com>
2024-08-02 10:16:55 +0900

    :white_check_mark: 구하림: 중간 저장

--- frontnext/src/app/page.tsx
+++ frontnext/src/app/page.tsx
@@ -1,16 +1,15 @@
-'use client';
-
 import Image from 'next/image';
-import HeaderSection from './ui/Header/headerSection';
+import Tabs from '../app/lib/tabs';
 import RenderLoginFormSection from './ui/RenderLoginFormSection';
 
 import Search from './lib/search';
-import SearchComponent from './lib/SearchComponent';
 export default function Home() {
   return (
-    <div>
-      <h1>hrim+숫자1~3 을 검색 하세요.</h1>
-      <SearchComponent />
+    <div className="search-test">
+      <header className="header">
+        <h1>검색하기</h1>
+        <Search />
+      </header>
     </div>
   );
 }
