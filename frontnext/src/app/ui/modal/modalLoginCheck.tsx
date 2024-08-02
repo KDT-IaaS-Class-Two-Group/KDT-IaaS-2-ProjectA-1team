00ec79abb0f1bd37b08f231685d4661c4b784957
김정수 <kjs20151240@gmail.com>
2024-08-02 11:38:22 +0900

    :recycle: 김정수 : literal값 삭제

--- frontnext/src/app/ui/modal/modalLoginCheck.tsx
+++ frontnext/src/app/ui/modal/modalLoginCheck.tsx
@@ -1,3 +1,46 @@
 'use client';
 
 import React from 'react';
+
+import ConDiv from '../Header/headerComponent/createConDiv';
+
+import { ModalStyles } from './modalStyle';
+
+import ButtonClick from '../Header/headerComponent/createBtn';
+
+import ModalPTag from './components/createPTag';
+
+interface LoginCheckProps {
+  clickFunc: () => void;
+  btnType: 'button';
+  Message: string;
+  BtnLiteral: string;
+}
+
+const LoginCheckModal: React.FC<LoginCheckProps> = ({
+  clickFunc,
+  btnType,
+  Message,
+  BtnLiteral,
+}) => {
+  return (
+    <>
+      <ConDiv className={ModalStyles.modalOverlay}>
+        <ConDiv className={ModalStyles.modalContent}>
+          <ModalPTag
+            textNode={Message}
+            className={ModalStyles.messageContainer}
+          />
+          <ButtonClick
+            type={btnType}
+            textNode={BtnLiteral}
+            clickFunc={clickFunc}
+            className={ModalStyles.closeButton}
+          />
+        </ConDiv>
+      </ConDiv>
+    </>
+  );
+};
+
+export default LoginCheckModal;
