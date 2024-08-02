5861242883ef14ffe2eb7dd18ad377cab9dab7e1
김정수 <kjs20151240@gmail.com>
2024-08-02 11:45:09 +0900

    :recycle: 김정수 : entry Point 정리

--- frontnext/src/app/page.tsx
+++ frontnext/src/app/page.tsx
@@ -1,16 +1,13 @@
 'use client';
 
 import Image from 'next/image';
-import HeaderSection from './ui/Header/headerSection';
-import RenderLoginFormSection from './ui/RenderLoginFormSection';
 
-import Search from './lib/search';
-import SearchComponent from './lib/SearchComponent';
+import LoginFormSection from './ui/loginFormSection/loginFormSection';
+
 export default function Home() {
   return (
-    <div>
-      <h1>hrim+숫자1~3 을 검색 하세요.</h1>
-      <SearchComponent />
-    </div>
+    <>
+      <LoginFormSection />
+    </>
   );
 }
