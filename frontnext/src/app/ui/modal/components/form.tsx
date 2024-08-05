65cea9df3e7bf4b34edda428a83740b3a3b7d570
정호연 <jea7730@gmail.com>
2024-08-05 15:36:33 +0900

    :fire: 정호연 : input tag 이동

--- frontnext/src/app/ui/modal/components/form.tsx
+++ frontnext/src/app/ui/modal/components/form.tsx
@@ -5,11 +5,6 @@ export const CreateTableForm = () => {
   return (
     <div className="p-4">
       <form action="/createTable" method="post" className="mt-4">
-        <input
-          type="text"
-          placeholder="테이블 이름을 입력하세요."
-          className="border rounded-lg p-2 w-full mb-4"
-        />
         <AddSets />
       </form>
     </div>
