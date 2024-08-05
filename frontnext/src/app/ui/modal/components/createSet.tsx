2a99faba5ce4371e9a01457d96414d7744df44f9
정호연 <jea7730@gmail.com>
2024-08-05 17:44:54 +0900

    :egg: 정호연 : 조합 입력 무시 이벤트 추가

--- frontnext/src/app/ui/modal/components/createSet.tsx
+++ frontnext/src/app/ui/modal/components/createSet.tsx
@@ -9,9 +9,9 @@ export const createSetJSX = (
   ) => void,
   containerRef: React.RefObject<HTMLDivElement>,
   setCount: React.Dispatch<React.SetStateAction<number>>,
-  handleKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void,
-  handleCompositionStart: () => void,
-  handleCompositionEnd: () => void,
+  handleKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void, // 추가된 인수
+  handleCompositionStart: () => void, // 추가된 인수
+  handleCompositionEnd: () => void, // 추가된 인수
 ) => {
   return (
     <div key={id} id={`set-${id}`} className="flex items-center mb-2.5 w-full">
@@ -20,10 +20,10 @@ export const createSetJSX = (
         placeholder="항목을 입력하세요."
         id={`item-${id}`}
         className="flex-grow border rounded-lg p-2 text-sm mr-2"
-        onKeyDown={handleKeyDown}
-        onCompositionStart={handleCompositionStart}
-        onCompositionEnd={handleCompositionEnd}
-        autoComplete="off"
+        onKeyDown={handleKeyDown} // 수정된 부분: handleKeyDown 추가
+        onCompositionStart={handleCompositionStart} // 추가된 부분: handleCompositionStart 추가
+        onCompositionEnd={handleCompositionEnd} // 추가된 부분: handleCompositionEnd 추가
+        autoComplete="off" // 자동 완성 기능 비활성화
       />
       <button
         onClick={() => handleDelete(id, containerRef, setCount)}
