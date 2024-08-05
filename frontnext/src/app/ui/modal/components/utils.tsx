1b2385eca107e98d9567d9d14c1c536cda052379
정호연 <jea7730@gmail.com>
2024-08-05 16:56:46 +0900

    :egg: 정호연 : 수정 완료

---
+++ frontnext/src/app/ui/modal/components/utils.tsx
@@ -0,0 +1,37 @@
+import React, { RefObject } from 'react';
+
+// 타입을 정의합니다.
+type SetCountFunction = (value: React.SetStateAction<number>) => void;
+
+export const handleDelete = (
+  id: number,
+  containerRef: RefObject<HTMLDivElement>,
+  setCount: SetCountFunction,
+) => {
+  const element = document.getElementById(`set-${id}`);
+  if (element) {
+    element.remove();
+    updateLabels(containerRef, setCount);
+  }
+};
+
+export const updateLabels = (
+  containerRef: RefObject<HTMLDivElement>,
+  setCount: SetCountFunction,
+) => {
+  if (!containerRef.current) {
+    return; // containerRef.current가 null일 경우 함수를 종료합니다.
+  }
+  const remainingSets = containerRef.current.querySelectorAll('div');
+  remainingSets.forEach((set, index) => {
+    const input = set.querySelector('input');
+    const button = set.querySelector('button');
+    const newId = index + 1;
+    if (input) input.id = `item-${newId}`;
+    if (button) {
+      button.onclick = () => handleDelete(newId, containerRef, setCount);
+    }
+    set.id = `set-${newId}`;
+  });
+  setCount(remainingSets.length);
+};
