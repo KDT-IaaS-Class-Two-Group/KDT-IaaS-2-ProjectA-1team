69213c144343417c012b7a9b7733c59b35fd1871
구하림 <166689288+koohrim12@users.noreply.github.com>
2024-08-05 15:59:47 +0900

    구하림 : page.tsx 원상복구 한 커밋

--- frontnext/src/app/page.tsx
+++ frontnext/src/app/page.tsx
@@ -1,22 +1,13 @@
-'use client'; // 클라이언트 컴포넌트로 설정
+'use client';
 
 import Image from 'next/image';
+
 import LoginFormSection from './ui/loginFormSection/loginFormSection';
-import SimpleApp from './lib/UserInput';
-import SelectBox from './lib/TableSelectBox'; // 클라이언트 컴포넌트 import
 
 export default function Home() {
-  const handleSelectChange = (value: string) => {
-    console.log('Selected value:', value);
-  };
-
   return (
     <>
-      <SelectBox
-        label="Select an option"
-        options={['Option 1', 'Option 2', 'Option 3']}
-        onChange={handleSelectChange}
-      />
+      <LoginFormSection />
     </>
   );
 }
