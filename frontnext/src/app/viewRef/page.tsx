209a9cebf1e18c7a0c9dfb6e13732b8b09c8cc7f
송이현 <songehyun00@gmail.com>
2024-08-05 16:05:41 +0900

    :memo: 합계부분 viewRef > page.tsx에 추가

--- frontnext/src/app/viewRef/page.tsx
+++ frontnext/src/app/viewRef/page.tsx
@@ -1,14 +1,20 @@
 'use client';
 
+import React from 'react';
 import HeaderSection from '../ui/Header/headerSection';
 import ToggleSection from '../ui/Toggle/ToggleSection';
+import SumComponent from '../ui/Sumsection/Sum';
 
 export default function Home() {
+  const selectedTableData = ['Item: 2000원', 'Item: 3000원'];
+  //특정 table을 읽어와서 ㅌ
+
   return (
     <div>
       <HeaderSection />
       <main>
         <ToggleSection />
+        <SumComponent selectedTableData={selectedTableData} />
       </main>
     </div>
   );
