9e1bca4d05c85e841231626620f5e5bf1e640b27
정호연 <jea7730@gmail.com>
2024-08-06 12:14:16 +0900

    :fire: 정호연 : form action, method 삭제

--- frontnext/src/app/ui/Modal-Test/components/CreateTableForm.tsx
+++ frontnext/src/app/ui/Modal-Test/components/CreateTableForm.tsx
@@ -4,9 +4,7 @@ import { AddSets } from './AddSets';
 export const CreateTableForm = () => {
   return (
     <div className="p-4">
-      <form action="/createTable" method="post" className="mt-4">
-        <AddSets />
-      </form>
+      <AddSets />
     </div>
   );
 };
