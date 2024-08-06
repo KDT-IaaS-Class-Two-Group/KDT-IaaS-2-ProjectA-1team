726514a73b7031e405e14290ac1f057d9d0f1d56
정호연 <jea7730@gmail.com>
2024-08-06 17:59:45 +0900

    :construction_worker: 정호연 : Modal folder

---
+++ frontnext/src/app/ui/Modal/utils/CreateSet.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { InputField } from '../components/InputField';
+
+export const createSetJSX = (
+  id: number,
+  handleDelete: (
+    id: number,
+    containerRef: React.RefObject<HTMLDivElement>,
+    setCount: React.Dispatch<React.SetStateAction<number>>,
+  ) => void,
+  containerRef: React.RefObject<HTMLDivElement>,
+  setCount: React.Dispatch<React.SetStateAction<number>>,
+  handleKeyDown: (e: React.KeyboardEvent<HTMLInputElement>, id: number) => void,
+  handleCompositionStart: () => void,
+  handleCompositionEnd: () => void,
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  value: string,
+  error: string,
+) => {
+  return (
+    <InputField
+      key={id}
+      id={id}
+      value={value}
+      error={error}
+      onKeyDown={handleKeyDown}
+      onCompositionStart={handleCompositionStart}
+      onCompositionEnd={handleCompositionEnd}
+      onChange={handleInputChange}
+      handleDelete={handleDelete}
+      containerRef={containerRef}
+      setCount={setCount}
+    />
+  );
+};
