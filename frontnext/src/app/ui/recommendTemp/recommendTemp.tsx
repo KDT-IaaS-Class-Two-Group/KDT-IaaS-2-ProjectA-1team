d33c3dc90a8317f9ea95cd0cb0ea00183752b55a
김정수 <kjs20151240@gmail.com>
2024-08-06 12:38:49 +0900

    :construction_worker: 김정수 : 기능 점검

--- frontnext/src/app/ui/recommendTemp/recommendTemp.tsx
+++ frontnext/src/app/ui/recommendTemp/recommendTemp.tsx
@@ -1,25 +1,25 @@
-'use client';
-/**
- * 일단 이건 모달창의 형태. 아니 children으로 들어가야 할 부분이다.
- * children을 핸들링 할 수 있어야 할 듯? chdilren이 변경되는 형태로 클릭이벤트로 useState로 true false값을 핸들링하자
- * useEffect? useCallback? 어느게 더 알맞은 방법이지?
- * 일단 틀만 만들어볼까?
- */
-import React, { useState, useEffect, ReactNode } from 'react';
-import ButtonClick from '../Header/headerComponent/createBtn';
+import React from 'react';
+import ButtonClick from '@/app/ui/Header/headerComponent/createBtn';
 import { callApi } from '@/app/lib/AJAX';
 
 interface RecommendProps {
   className?: string;
-  onclick?: () => void;
+  onclick?: (text: string) => void;
 }
 
-// 이 return값은 자식으로 들어가는 것을 상정한다. 그렇다면?
 const RecommendTemp: React.FC<RecommendProps> = ({ className, onclick }) => {
-  const copyToRecommned = () => {
-    const useData = {};
+  // 버튼 클릭 시 호출되는 함수
+  const handleButtonClick = async (text: string) => {
+    // useData를 생성
+    const useData = { table: text };
 
-    const response = callApi('8000/createRecommend', 'POST', useData);
+    try {
+      // API 호출
+      const response = await callApi('8000/createRecommend', 'POST', useData);
+      console.log(response); // 응답을 콘솔에 출력
+    } catch (error) {
+      console.error('API 호출 중 오류 발생:', error);
+    }
   };
 
   return (
@@ -28,20 +28,15 @@ const RecommendTemp: React.FC<RecommendProps> = ({ className, onclick }) => {
         type="button"
         className={className}
         textNode="요식업"
-        clickFunc={onclick}
+        clickFunc={() => handleButtonClick('요식업')}
       />
       <ButtonClick
         type="button"
         className={className}
         textNode="인력부"
-        clickFunc={onclick}
+        clickFunc={() => handleButtonClick('인력부')}
       />
-      <ButtonClick
-        type="button"
-        className={className}
-        textNode="돌아가기"
-        clickFunc={onclick}
-      />
+      <ButtonClick type="button" className={className} textNode="돌아가기" />
     </>
   );
 };
