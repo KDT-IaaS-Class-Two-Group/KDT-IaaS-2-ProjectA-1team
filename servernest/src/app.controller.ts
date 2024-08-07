bc2088c58b9e3eea91883bd4db810b6e46c9779b
김정수 <kjs20151240@gmail.com>
2024-08-07 10:07:31 +0900

    :sparkles: 김정수 : createRecommend요청 추가

--- servernest/src/app.controller.ts
+++ servernest/src/app.controller.ts
@@ -40,10 +40,6 @@ export class AppController {
 
     try {
       const response = await fetch('http://localhost:8080/createTable', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
         body: JSON.stringify(createTableDto),
       });
 
@@ -58,6 +54,23 @@ export class AppController {
     } catch (error) {
       console.error('Python 서버 요청 중 오류 발생:', error);
       res.status(500).json({ message: 'Python 서버 요청 중 오류 발생' });
+    }
+  }
+
+  @Post('/createRecommend')
+  async makeResponse(@Body() body: string, @Res() res: Response) {
+    try {
+      const response = await fetch('http://localhost:8080/createRecommend', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await response.json();
+      res.status(response.status).send(data.success);
+    } catch (error) {
+      res.status(error.response?.status || 500).send(error.message || 'Error');
     }
   }
 }
