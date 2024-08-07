922c25efcd1b85beaea44944cc289c8180c9fd60
김정수 <kjs20151240@gmail.com>
2024-08-07 11:29:25 +0900

    :egg: props수정 및 할당 방식 변경

--- frontnext/src/app/ui/recommendTemp/recommendTemp.tsx
+++ frontnext/src/app/ui/recommendTemp/recommendTemp.tsx
@@ -6,34 +6,23 @@ import { callApi } from '@/app/lib/AJAX';
 
 interface RecommendProps {
   className?: string;
-  onClose?: () => void;
+  onClose?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const RecommendTemp: React.FC<RecommendProps> = ({ className, onClose }) => {
-  const handleButtonClick = async (text: string) => {
-    const useData = { table: text };
-
-    try {
-      const response = await callApi('8000/createRecommend', 'POST', useData);
-      console.log(response);
-    } catch (error) {
-      console.error('API 호출 중 오류 발생:', error);
-    }
-  };
-
   return (
     <>
       <ButtonClick
         type="button"
         className={className}
         textNode="요식업"
-        clickFunc={() => handleButtonClick('요식업')}
+        clickFunc={onClose}
       />
       <ButtonClick
         type="button"
         className={className}
         textNode="인력부"
-        clickFunc={() => handleButtonClick('인력부')}
+        clickFunc={onClose}
       />
       <ButtonClick
         type="button"
