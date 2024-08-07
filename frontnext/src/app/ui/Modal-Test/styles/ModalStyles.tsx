14259be87fb1b8b000ad95467669a3e48a01fe2e
정호연 <jea7730@gmail.com>
2024-08-07 11:30:57 +0900

    :art: 정호연 : 수정

--- frontnext/src/app/ui/Modal-Test/styles/ModalStyles.tsx
+++ frontnext/src/app/ui/Modal-Test/styles/ModalStyles.tsx
@@ -4,17 +4,6 @@ export const ModalStyles = {
     'fixed inset-0 flex items-center justify-center bg-black bg-opacity-50',
   modalContent: 'bg-white p-6 rounded-lg relative max-w-lg w-full mx-4',
   closeButton: 'absolute top-2 right-2 text-gray-500 hover:text-gray-700',
-  input:
-    'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
-  form: 'bg-white p-6 rounded shadow-lg w-full max-w-sm',
-  button:
-    'w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
-  container:
-    'flex flex-col items-center justify-center min-h-screen bg-gray-100',
-  label: 'block text-sm font-medium text-gray-700',
-  messageContainer: 'text-center',
-  successText: 'text-blue-500 text-4xl font-bold',
-  failureText: 'text-red-500 text-4xl font-bold',
 };
 
 export const LoginCheckCardStyles = {
@@ -42,10 +31,6 @@ const CreateTableStyle = {
   addButton: 'bg-blue-500',
   createButton: 'bg-green-500 hover:bg-green-600',
   inputError: 'border-red-500',
-  modalOverlay:
-    'fixed inset-0 flex items-center justify-center bg-black bg-opacity-50',
-  modalContent: 'bg-white p-6 rounded-lg relative max-w-lg w-full mx-4',
-  closeButton: 'absolute top-2 right-2 text-gray-500 hover:text-gray-700',
 };
 
 export default CreateTableStyle;
