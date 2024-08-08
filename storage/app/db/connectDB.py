302dd4c6a3941df8ff6ecba9377ae3de4fd7ad6c
김정수 <kjs20151240@gmail.com>
2024-08-08 10:34:01 +0900

    :sparkles: 김정수 : conn을 return하는 기능 connect_db

--- storage/app/db/connectDB.py
+++ storage/app/db/connectDB.py
@@ -1,25 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import sqlite3
 
-DATABASE_URL = "sqlite:///./backup/backupstorage.db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal= sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, index=True)
-
-def connect_db():
-    db = SessionLocal()
-    try : 
-        Base.metadata.create_all(bind=engine)
-        yield db
-    finally:
-        print('Database created')
-        db.close()
+def connect_db(storage):
+    conn = sqlite3.connect(storage)
+    conn.row_factory = sqlite3.Row
+    return conn
