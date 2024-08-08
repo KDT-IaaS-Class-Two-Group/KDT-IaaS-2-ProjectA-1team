e2febde99ed8c8e8e2c888a2f2d1cc85cd0dedb5
김정수 <kjs20151240@gmail.com>
2024-08-08 10:33:16 +0900

    :construction_worker: 김정수 : db생성 테스트

--- storage/app/main.py
+++ storage/app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from db.connectDB import connect_db
 
 app = FastAPI();
 
 
 @app.get('/')
 def helloThere():
+    DBPATH = "./app/backup/backupstorage.db"
     man = "world"
     print(f"hello there my name is {man}")
+    conn = connect_db(DBPATH)
+    try : 
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS eest (id INTEGER, name TEXT)")
+        cursor.execute("INSERT INTO test (id, name) VALUES (?, ?)",("1","TEST"))
+        conn.commit()
+    finally : 
+        conn.close()
 
 if __name__ == "__main__" :
     import uvicorn
