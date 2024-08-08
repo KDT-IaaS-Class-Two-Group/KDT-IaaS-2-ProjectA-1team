2c393ddbfd719dc8c1a7b65573089b6289aab7d3
이연승 <leeys951006@gmail.com>
2024-08-08 14:19:30 +0900

    :memo: 이연승 : page.tsx 파일 수정

--- frontnext/src/app/InsertPage/page.tsx
+++ frontnext/src/app/InsertPage/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image';
 import HeaderSection from '../ui/Header/headerSection';
 import TotalSidebar from '../ui/Modal-Test/TotalSidebar';
+import EditList from '../lib/EditList';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <HeaderSection />
+      <EditList />
       <TotalSidebar />
-    </>
+
+    </div>
+
   );
 }
