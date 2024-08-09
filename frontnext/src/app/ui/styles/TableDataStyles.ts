dd7a646ef622c51b9fa0845c26c856edfa9c801e
정호연 <jea7730@gmail.com>
2024-08-09 23:46:07 +0900

    :art: 정호연 : input 개수 너비

--- frontnext/src/app/ui/styles/TableDataStyles.ts
+++ frontnext/src/app/ui/styles/TableDataStyles.ts
@@ -13,6 +13,8 @@ const TableStyles = {
   deleteRowButton:
     'absolute left-0 top-1/2 transform -translate-y-1/2 bg-red-500 text-white text-sm rounded-full w-6 h-6 flex items-center justify-center hover:bg-red-600', // 정사각형으로 설정
   errorText: 'text-red-500 text-xs mt-1', // 오류 메시지 스타일
+  tableWrapper: 'min-w-full overflow-x-auto', // 테이블 영역에 가로 스크롤 처리
+  inputWide: 'w-32 px-2 py-1 border rounded', // 8개 이상일 때 적용할 넓은 인풋 너비
 };
 
 export default TableStyles;
