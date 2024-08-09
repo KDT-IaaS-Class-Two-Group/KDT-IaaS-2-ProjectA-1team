a1568f5bbd2e234d87f6ade57088c6f6210c12cb
정호연 <jea7730@gmail.com>
2024-08-09 18:08:00 +0900

    :egg: Modal Component 사용

--- frontnext/src/app/ui/Modal-Test/components/ConfirmSaveModal.tsx
+++ frontnext/src/app/ui/Modal-Test/components/ConfirmSaveModal.tsx
@@ -1,5 +1,6 @@
 // components/ConfirmSaveModal.tsx
 import React from 'react';
+import Modal from '../modalComponent';
 import { ModalStyles } from '../styles/ModalStyles';
 
 interface ConfirmSaveModalProps {
@@ -13,30 +14,24 @@ const ConfirmSaveModal: React.FC<ConfirmSaveModalProps> = ({
   onConfirm,
   onClose,
 }) => {
-  if (!show) {
-    return null;
-  }
-
   return (
-    <div className={ModalStyles.modalOverlay}>
-      <div className={ModalStyles.modalContent}>
-        <h2 className="text-lg font-bold mb-4">저장하시겠습니까?</h2>
-        <div className="flex justify-end space-x-4">
-          <button
-            className={`${ModalStyles.button} ${ModalStyles.cancelButton}`}
-            onClick={onClose}
-          >
-            취소
-          </button>
-          <button
-            className={`${ModalStyles.button} ${ModalStyles.confirmButton}`}
-            onClick={onConfirm}
-          >
-            확인
-          </button>
-        </div>
+    <Modal show={show} onClose={onClose}>
+      <h2 className="text-lg font-bold mb-4">정말로 저장하시겠습니까?</h2>
+      <div className="flex justify-end space-x-4">
+        <button
+          className={`${ModalStyles.button} ${ModalStyles.cancelButton}`}
+          onClick={onClose}
+        >
+          취소
+        </button>
+        <button
+          className={`${ModalStyles.button} ${ModalStyles.confirmButton}`}
+          onClick={onConfirm}
+        >
+          확인
+        </button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
