23c5cae908285a7a84aa4094c7c33411e8990388
정호연 <jea7730@gmail.com>
2024-08-09 22:24:55 +0900

    :art: 정호연 : styles

--- frontnext/src/app/ui/styles/TableDataStyles.ts
+++ frontnext/src/app/ui/styles/TableDataStyles.ts
@@ -5,14 +5,14 @@ const TableStyles = {
   th: 'relative px-4 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-600',
   headerInput: 'w-full px-2 py-1 border rounded',
   deleteColumnButton:
-    'absolute top-0 right-0 transform -translate-y-1/2 mx-1 px-2 bg-red-500 text-white text-sm rounded',
-  errorText: 'text-red-500 text-sm mt-1', // 오류 메시지 스타일
+    'absolute top-1/2 right-1 transform -translate-y-1/2 bg-red-500 text-white text-sm rounded-full w-6 h-6 flex items-center justify-center hover:bg-red-600', // 정사각형으로 설정
   tbody: '',
   tr: 'hover:bg-gray-100',
   td: 'relative px-4 py-2 border-b border-gray-200 text-sm text-gray-700',
   rowInput: 'w-full px-2 py-1 border rounded',
   deleteRowButton:
-    'absolute left-0 top-1/2 transform -translate-y-1/2 mx-1 px-2 bg-red-500 text-white text-sm rounded',
+    'absolute left-0 top-1/2 transform -translate-y-1/2 bg-red-500 text-white text-sm rounded-full w-6 h-6 flex items-center justify-center hover:bg-red-600', // 정사각형으로 설정
+  errorText: 'text-red-500 text-xs mt-1', // 오류 메시지 스타일
 };
 
 export default TableStyles;
