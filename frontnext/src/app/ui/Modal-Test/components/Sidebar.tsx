94a23445308f4e80e321c4037c1bee47a879a415
정호연 <jea7730@gmail.com>
2024-08-09 12:26:28 +0900

    :sparkles: 정호연 : 열, 행 추가 컴포넌트 추가

--- frontnext/src/app/ui/Modal-Test/components/Sidebar.tsx
+++ frontnext/src/app/ui/Modal-Test/components/Sidebar.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import TableModal from './CreateTableModal';
 import TableList from './TableList';
+import AddRowButton from './AddRowButton';
+import AddColumnButton from './AddColumnButton';
 import SidebarStyles from '../styles/SidebarStyles';
 
 interface SidebarProps {
   onTableClick: (tableName: string) => void;
+  onAddRow: () => void;
+  onAddColumn: () => void;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ onTableClick }) => {
+const Sidebar: React.FC<SidebarProps> = ({
+  onTableClick,
+  onAddRow,
+  onAddColumn,
+}) => {
   return (
     <>
       <TableModal />
       <div className={SidebarStyles.sidebar}>
         <TableList onTableClick={onTableClick} />
+        <div className={SidebarStyles.buttonContainer}>
+          <AddRowButton onAddRow={onAddRow} />
+          <AddColumnButton onAddColumn={onAddColumn} />
+        </div>
       </div>
     </>
   );
