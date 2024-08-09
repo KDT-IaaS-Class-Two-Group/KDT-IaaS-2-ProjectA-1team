5a3a98b3b14d8e661ff19cd39cd512443ae4639f
구하림 <166689288+koohrim12@users.noreply.github.com>
2024-08-09 12:40:13 +0900

    :art: 구하림: 비밀번호변경 가운데로 정렬

--- frontnext/src/app/ui/styles/stylesKHR.ts
+++ frontnext/src/app/ui/styles/stylesKHR.ts
@@ -4,7 +4,8 @@ export const PwChangeModalStyles = {
     'fixed inset-0 flex items-center justify-center bg-black bg-opacity-50',
   modalContent: 'bg-white p-6 rounded-lg relative max-w-lg w-full mx-4',
   closeButton: 'absolute top-2 right-2 text-gray-500 hover:text-gray-700',
-  form: 'bg-white p-6 rounded shadow-lg w-full max-w-sm',
+  form: 'bg-white rounded shadow-lg w-full max-w-sm p-16 ml-8',
+
   input:
     'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
   button:
