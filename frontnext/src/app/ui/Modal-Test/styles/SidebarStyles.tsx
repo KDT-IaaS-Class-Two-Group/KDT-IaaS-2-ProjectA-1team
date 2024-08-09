10d15ac7861a258eb044e6dc7a5ec46f4fde6e84
정호연 <jea7730@gmail.com>
2024-08-09 12:24:05 +0900

    :art: 정호연 : 스타일 추가

--- frontnext/src/app/ui/Modal-Test/styles/SidebarStyles.tsx
+++ frontnext/src/app/ui/Modal-Test/styles/SidebarStyles.tsx
@@ -4,6 +4,15 @@ const SidebarStyles = {
   createButton: 'bg-blue-500',
   title: 'text-xl font-bold mb-4',
   tableItem: 'py-2 px-4 hover:bg-gray-700 cursor-pointer',
+  container: 'flex',
+  modalOverlay: 'text-center',
+  mainContent: 'flex-grow p-4',
+  mainTitle: 'text-2xl font-bold',
+  tableTitle: 'text-xl font-bold mb-4',
+  saveButton: 'mt-4 px-4 py-2 bg-blue-500 text-white rounded',
+  addRowButton: 'mt-4 ml-2 px-4 py-2 bg-green-500 text-white rounded',
+  addColumnButton: 'mt-4 ml-2 px-4 py-2 bg-green-500 text-white rounded',
+  buttonContainer: 'mt-4 space-y-2', // 새로운 속성 추가
 };
 
 export default SidebarStyles;
