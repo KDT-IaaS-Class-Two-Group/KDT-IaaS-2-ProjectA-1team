f4af62f8df97b14141bb07decb28c47d6cdd4213
정호연 <jea7730@gmail.com>
2024-08-12 13:51:31 +0900

    :art: 정호연 : styles edit

--- frontnext/src/app/ui/SettingMoules/ReportSettings.tsx
+++ frontnext/src/app/ui/SettingMoules/ReportSettings.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { SettingStyles } from '../styles/styles-ys';
+import TotalStyles from '../styles/TotalStyles';
 
 const ReportSettings = () => {
   return (
-    <div className={SettingStyles.contentStyle}>
-      <div className="flex flex-col space-y-4">
-        <button className={SettingStyles.button}>재고 보고서 형식 설정</button>
-        <button className={SettingStyles.button}>통계 데이터 표시 방법 설정</button>
-        <button className={SettingStyles.button}>보고서 자동 생성 및 스케줄링</button>
+    <div className={TotalStyles.SettingContentStyle}>
+      <div className={TotalStyles.SettingButtonContainer}>
+        <button className={TotalStyles.SettingButton}>
+          재고 보고서 형식 설정
+        </button>
+        <button className={TotalStyles.SettingButton}>
+          통계 데이터 표시 방법 설정
+        </button>
+        <button className={TotalStyles.SettingButton}>
+          보고서 자동 생성 및 스케줄링
+        </button>
       </div>
     </div>
   );
