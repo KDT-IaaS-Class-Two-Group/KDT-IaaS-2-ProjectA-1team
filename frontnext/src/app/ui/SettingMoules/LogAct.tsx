5a221741c33d7d34f943019f56645044321b2b1e
정호연 <jea7730@gmail.com>
2024-08-12 13:50:32 +0900

    :art: 정호연 : styles edit

--- frontnext/src/app/ui/SettingMoules/LogAct.tsx
+++ frontnext/src/app/ui/SettingMoules/LogAct.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { SettingStyles } from '../styles/styles-ys';
+import TotalStyles from '../styles/TotalStyles';
 
 const LogAct = () => {
   return (
-    <div className={SettingStyles.contentStyle}>
-    
-      <div className="flex flex-col space-y-4">
-        <button className={SettingStyles.button}>사용자 활동 로그 조회</button>
-        <button className={SettingStyles.button}>로그 파일 저장 및 다운로드</button>
+    <div className={TotalStyles.SettingContentStyle}>
+      <div className={TotalStyles.SettingButtonContainer}>
+        <button className={TotalStyles.SettingButton}>
+          사용자 활동 로그 조회
+        </button>
+        <button className={TotalStyles.SettingButton}>
+          로그 파일 저장 및 다운로드
+        </button>
       </div>
     </div>
   );
