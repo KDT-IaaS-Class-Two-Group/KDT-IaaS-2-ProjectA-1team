715b7858c8ac07d206047bdeaebaca2ebcec1471
정호연 <jea7730@gmail.com>
2024-08-12 13:53:03 +0900

    :art: 정호연 : styles edit

--- frontnext/src/app/ui/SettingMoules/Sidebar.tsx
+++ frontnext/src/app/ui/SettingMoules/Sidebar.tsx
@@ -1,20 +1,56 @@
 import React from 'react';
-import { SettingStyles } from '../styles/styles-ys';
+import TotalStyles from '../styles/TotalStyles';
 
-const Sidebar = ({ isOpen, setSelectedMenu }: { isOpen: boolean, setSelectedMenu: (menu: string) => void }) => {
+const Sidebar = ({
+  isOpen,
+  setSelectedMenu,
+}: {
+  isOpen: boolean;
+  setSelectedMenu: (menu: string) => void;
+}) => {
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div className={SettingStyles.sidebar}>
+    <div className={TotalStyles.SettingSidebar}>
       <ul>
-        <li className={SettingStyles.button} onClick={() => setSelectedMenu('userManagement')}>사용자 관리</li>
-        <li className={SettingStyles.button} onClick={() => setSelectedMenu('logAct')}>로그 및 활동 추적</li>
-        <li className={SettingStyles.button} onClick={() => setSelectedMenu('interfaceSettings')}>인터페이스 설정</li>
-        <li className={SettingStyles.button} onClick={() => setSelectedMenu('dataImportExport')}>데이터 임포트/익스포트</li>
-        <li className={SettingStyles.button} onClick={() => setSelectedMenu('reportSettings')}>통계 및 보고서 설정</li>
-        <li className={SettingStyles.button} onClick={() => setSelectedMenu('customerSupportSettings')}>고객 지원 설정</li>
+        <li
+          className={TotalStyles.SettingButton}
+          onClick={() => setSelectedMenu('userManagement')}
+        >
+          사용자 관리
+        </li>
+        <li
+          className={TotalStyles.SettingButton}
+          onClick={() => setSelectedMenu('logAct')}
+        >
+          로그 및 활동 추적
+        </li>
+        <li
+          className={TotalStyles.SettingButton}
+          onClick={() => setSelectedMenu('interfaceSettings')}
+        >
+          인터페이스 설정
+        </li>
+        <li
+          className={TotalStyles.SettingButton}
+          onClick={() => setSelectedMenu('dataImportExport')}
+        >
+          데이터 임포트/익스포트
+        </li>
+        <li
+          className={TotalStyles.SettingButton}
+          onClick={() => setSelectedMenu('reportSettings')}
+        >
+          통계 및 보고서 설정
+        </li>
+        <li
+          className={TotalStyles.SettingButton}
+          onClick={() => setSelectedMenu('customerSupportSettings')}
+        >
+          고객 지원 설정
+        </li>
       </ul>
     </div>
   );
