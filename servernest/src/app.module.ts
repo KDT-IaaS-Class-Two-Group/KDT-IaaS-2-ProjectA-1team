3970a0d97e8374f7a90754dfc492eadae2e60fca
김정수 <kjs20151240@gmail.com>
2024-08-12 12:35:30 +0900

    :egg: 김정수 : backCotroller추가

--- servernest/src/app.module.ts
+++ servernest/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchDataController } from './searchdata/searchdata.controller';
+import { BackController } from './back/back.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, SearchDataController],
+  controllers: [AppController, SearchDataController, BackController],
   providers: [AppService],
 })
 export class AppModule {}
