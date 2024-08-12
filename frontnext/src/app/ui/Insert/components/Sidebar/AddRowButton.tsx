b39ccb8577faf980677796f9695558c36ba9eeae
이연승 <leeys951006@gmail.com>
2024-08-12 10:51:29 +0900

    :memo: 이연승 : 인터페이스 언어 설정 구현

--- frontnext/src/app/ui/Insert/components/Sidebar/AddRowButton.tsx
+++ frontnext/src/app/ui/Insert/components/Sidebar/AddRowButton.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import SidebarStyles from '../../styles/SidebarStyles';
+import { useLanguage } from '../../../SettingMoules/LanguageContext';
 
 interface AddRowButtonProps {
   onAddRow: () => void;
 }
 
 const AddRowButton: React.FC<AddRowButtonProps> = ({ onAddRow }) => {
+  const { language } = useLanguage();
+  const texts = {
+    ko: '행 추가',
+    en: 'Add Row',
+    jp: '行を追加',
+    cn: '添加行',
+    vn: 'Thêm hàng',
+    th: 'เพิ่มแถว',
+  };
+
   return (
     <button
       className={`${SidebarStyles.button} ${SidebarStyles.addRowButton}`}
       onClick={onAddRow}
     >
-      행 추가
+      {texts[language]}
     </button>
   );
 };
