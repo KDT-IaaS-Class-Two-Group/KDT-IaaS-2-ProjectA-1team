e64064afdbe07a5da923d8607d392404219d89ce
이연승 <leeys951006@gmail.com>
2024-08-12 09:58:22 +0900

    :memo: 이연승 : 인터페이스 언어 설정 구현

--- frontnext/src/app/InsertPage/page.tsx
+++ frontnext/src/app/InsertPage/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import HeaderSection from '../ui/Header/headerSection';
 import TotalSidebar from '../ui/Modal-Test/TotalInsert';
+import { LanguageProvider } from '../ui/SettingMoules/LanguageContext';
 
 export default function Home() {
   return (
-    <>
+    <LanguageProvider>
       <HeaderSection />
       <TotalSidebar />
-    </>
+    </LanguageProvider>
   );
 }
