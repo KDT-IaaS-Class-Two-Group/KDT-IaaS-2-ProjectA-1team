8600978b6e8b91b6f9b57464ae93f3c889d3c937
정호연 <jea7730@gmail.com>
2024-08-12 13:48:22 +0900

    :art: 정호연 : styles edit

--- frontnext/src/app/ui/SettingMoules/DataImportExport.tsx
+++ frontnext/src/app/ui/SettingMoules/DataImportExport.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { SettingStyles } from '../styles/styles-ys';
+import TotalStyles from '../styles/TotalStyles';
 
 const DataImportExport = () => {
   return (
-    <div className={SettingStyles.contentStyle}>
-
-      <div className="flex flex-col space-y-4">
-        <button className={SettingStyles.button}>재고 데이터 임포트 (CSV)</button>
-        <button className={SettingStyles.button}>재고 데이터 익스포트 (CSV)</button>
-        <button className={SettingStyles.button}>재고 데이터 임포트 (Excel)</button>
-        <button className={SettingStyles.button}>재고 데이터 익스포트 (Excel)</button>
+    <div className={TotalStyles.SettingContentStyle}>
+      <div className={TotalStyles.SettingButtonContainer}>
+        <button className={TotalStyles.SettingButton}>
+          재고 데이터 임포트 (CSV)
+        </button>
+        <button className={TotalStyles.SettingButton}>
+          재고 데이터 익스포트 (CSV)
+        </button>
+        <button className={TotalStyles.SettingButton}>
+          재고 데이터 임포트 (Excel)
+        </button>
+        <button className={TotalStyles.SettingButton}>
+          재고 데이터 익스포트 (Excel)
+        </button>
       </div>
     </div>
   );
