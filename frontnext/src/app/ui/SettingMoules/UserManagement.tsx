0e087806b67642ce96ebd68a9b01f1f609e4559b
정호연 <jea7730@gmail.com>
2024-08-12 13:54:19 +0900

    :art: 정호연 : styles edit

--- frontnext/src/app/ui/SettingMoules/UserManagement.tsx
+++ frontnext/src/app/ui/SettingMoules/UserManagement.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { SettingStyles } from '../styles/styles-ys';
+import TotalStyles from '../styles/TotalStyles';
 import PasswordChangeForm from './PWChangeForm';
 
 const UserManagement = () => {
   return (
-    <div className={SettingStyles.contentStyle}>
+    <div className={TotalStyles.SettingContentStyle}>
       <div className="flex flex-col space-y-4">
-        <button className={SettingStyles.button}>사용자 계정 생성 및 삭제</button>
-        <button className={SettingStyles.button}>사용자 권한 설정</button>
+        <button className={TotalStyles.SettingButton}>
+          사용자 계정 생성 및 삭제
+        </button>
+        <button className={TotalStyles.SettingButton}>사용자 권한 설정</button>
         <PasswordChangeForm />
-        <button className={SettingStyles.button}>알림 설정</button>
-        <button className={SettingStyles.button}>백업 및 복구</button>
-        
+        <button className={TotalStyles.SettingButton}>알림 설정</button>
+        <button className={TotalStyles.SettingButton}>백업 및 복구</button>
       </div>
     </div>
   );
