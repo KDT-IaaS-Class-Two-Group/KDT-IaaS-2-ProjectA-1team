b58c60e53e11300cebf7faa7ac0dc566e8079e3a
김정수 <kjs20151240@gmail.com>
2024-08-12 11:27:22 +0900

    :recycle: 김정수 : saveData모델 지정

--- storage/app/models/backup_models.py
+++ storage/app/models/backup_models.py
@@ -1,1 +1,20 @@
-print("backup_models.py")
+from pydantic import BaseModel
+from typing import List, Dict, Union, Optional
+
+# 테이블 스키마를 정의하는 모델
+class TableSchema(BaseModel):
+    cid: int
+    name: str
+    type: str
+    notnull: int
+    dflt_value: Optional[Union[str, int]] = None
+    pk: int
+
+# 테이블 데이터를 정의하는 모델
+class TableData(BaseModel):
+    schema: List[TableSchema]
+    data: List[List[Union[str, int, float, None]]]
+
+# 전체 백업 데이터를 정의하는 모델
+class SaveData(BaseModel):
+    backup_data: Dict[str, TableData]
