6df81fd787edf5e76ef63fd758ad282ca9bfe25c
정호연 <jea7730@gmail.com>
2024-08-12 13:49:41 +0900

    :art: 정호연 : styles edit

--- frontnext/src/app/ui/SettingMoules/InterfaceSettings.tsx
+++ frontnext/src/app/ui/SettingMoules/InterfaceSettings.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { SettingStyles } from '../styles/styles-ys';
+import TotalStyles from '../styles/TotalStyles';
 
 const InterfaceSettings = () => {
   return (
-    <div className={SettingStyles.contentStyle}>
-      <div className="flex flex-col space-y-4">
-        <button className={SettingStyles.button}>테마 변경 (다크 모드, 라이트 모드)</button>
-        <button className={SettingStyles.button}>인터페이스 언어 설정</button>
+    <div className={TotalStyles.SettingContentStyle}>
+      <div className={TotalStyles.SettingButtonContainer}>
+        <button className={TotalStyles.SettingButton}>
+          테마 변경 (다크 모드, 라이트 모드)
+        </button>
+        <button className={TotalStyles.SettingButton}>
+          인터페이스 언어 설정
+        </button>
       </div>
     </div>
   );
