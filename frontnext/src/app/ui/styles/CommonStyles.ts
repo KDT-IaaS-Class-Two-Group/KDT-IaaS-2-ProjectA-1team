a2ac5fabe4c9d86596fdf392a073d0b1af8fb564
정호연 <jea7730@gmail.com>
2024-08-14 12:40:40 +0900

    :rocket: 정호연 : common styles file

---
+++ frontnext/src/app/ui/styles/CommonStyles.ts
@@ -0,0 +1,28 @@
+const common = {
+  // 공통 스타일
+  base: 'px-4 py-2 rounded-lg text-white', // 기본적인 버튼 스타일
+  hoverEffect: 'hover:bg-opacity-75', // 모든 버튼에 적용할 호버 효과
+  roundedFull: 'rounded-full',
+  roundedLrage: 'rounded-lg',
+  shadowEffect: 'shadow-sm',
+  absolute: 'absolute', // 절대 위치
+  action: 'transition duration-300', // 애니메이션
+
+  // 여백 스타일
+  px: 'px-4',
+  py: 'py-2',
+  mb: 'mb-4',
+
+  // 색상 스타일
+  blue: 'bg-blue-500 hover:bg-blue-600',
+  gray: 'bg-gray-500 hover:bg-gray-600',
+  red: 'bg-red-500 hover:bg-red-600',
+  green: 'bg-green-500 hover:bg-green-600',
+
+  // 글자 스타일
+  textGray: 'text-gray-500 hover:text-gray-600',
+  textBlack: 'text-black',
+  textWhite: 'text-white',
+};
+
+export default common;
