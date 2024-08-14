57aca3c349775f07d7eaf57ce15a731dddde87de
정호연 <jea7730@gmail.com>
2024-08-14 11:03:47 +0900

    :egg: 정호연 : Modal bg styles 수정

--- frontnext/src/app/ui/styles/TotalStyles.ts
+++ frontnext/src/app/ui/styles/TotalStyles.ts
@@ -1,7 +1,7 @@
 const TotalStyles = {
   // Modal Styles
   ModalOverlay:
-    'fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50',
+    'fixed inset-0 flex items-center justify-center bg-black z-50 bg-opacity-50 backdrop-blur-md',
   ModalContent: 'bg-white p-6 rounded-lg relative max-w-lg w-full mx-4',
   ModalCloseButton: 'absolute top-2 right-2 text-gray-500 hover:text-gray-700',
   ModalButton: 'px-4 py-2 rounded-full text-center text-white', // 버튼의 세로 크기를 맞추기 위해 text-center 추가
@@ -9,7 +9,7 @@ const TotalStyles = {
   ModalConfirmButton: 'bg-blue-500 hover:bg-blue-600',
   ModalText: 'text-gray-800 text-lg', // 텍스트 색상과 크기를 설정
   ConfirmButton:
-    'bg-blue-500 hover:bg-blue-600 mt-4 px-4 py-2 rounded-lg text-white',
+    'bg-blue-500 hover:bg-blue-600 mt-4 px-4 py-2 rounded-full text-white',
 
   // Create Table Styles
   CreateTableContainer: 'flex flex-col items-start mb-2.5 w-full',
