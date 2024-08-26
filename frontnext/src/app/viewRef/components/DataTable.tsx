d6746048fe3245daf3c1f9ca060330a0d12b59e7
이연승 <leeys951006@gmail.com>
2024-08-26 12:54:05 +0900

    :memo: 이연승 : DataTable.tsx 모듈 작업

--- frontnext/src/app/viewRef/components/DataTable.tsx
+++ frontnext/src/app/viewRef/components/DataTable.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import TotalStyles from '../../ui/styles/TotalStyles';
+import TableHeader from '../components/Table/TableHeader';
+import TableBody from '../components/Table/TableBody';
+import TableContainer from '../components/Table/TableContainer';
 
 interface DataTableProps {
   data: { [key: string]: string | number }[];
@@ -10,49 +12,17 @@ const DataTable: React.FC<DataTableProps> = ({ data }) => {
   const isScrollable = columnCount >= 8; // 열 개수가 8개 이상인지 확인
 
   return (
-    <div className={TotalStyles.ToggleTableContainer}>
+    <TableContainer isScrollable={isScrollable} columnCount={columnCount}>
       {data.length > 0 && (
-        <div
-          className={
-            data.length > 15 ? TotalStyles.ToggleTableScrollWrapper : ''
-          }
-        >
-          <table
-            className={TotalStyles.ToggleTable}
-            style={isScrollable ? { minWidth: columnCount * 150 } : {}}
-          >
-            <thead className={TotalStyles.ToggleTableHead}>
-              <tr>
-                {Object.keys(data[0]).map((key) => (
-                  <th
-                    key={key}
-                    className={TotalStyles.ToggleTableHeaderCell}
-                    style={isScrollable ? { minWidth: '250px' } : {}}
-                  >
-                    {key}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody className={TotalStyles.ToggleTableBody}>
-              {data.map((item, index) => (
-                <tr key={index}>
-                  {Object.entries(item).map(([key, value]) => (
-                    <td
-                      key={key}
-                      className={TotalStyles.ToggleTableDataCell}
-                      style={isScrollable ? { minWidth: '250px' } : {}}
-                    >
-                      {value}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <>
+          <TableHeader
+            keys={Object.keys(data[0])}
+            isScrollable={isScrollable}
+          />
+          <TableBody data={data} isScrollable={isScrollable} />
+        </>
       )}
-    </div>
+    </TableContainer>
   );
 };
 
